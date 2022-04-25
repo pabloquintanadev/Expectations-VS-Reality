14e317795ecac41895bf573b1e764230d8add3ff
mdelgado333 <101524023+mdelgado333@users.noreply.github.com>
2022-04-25 12:45:24 +0200

    doooooooone

--- models/Message.model.js
+++ models/Message.model.js
@@ -3,16 +3,20 @@ const { Schema, model } = require("mongoose");
 const messageSchema = new Schema(
   {
     from: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     to: {
-        type: String,
-        unique: true
+      type: String,
+      unique: true
     },
     textContent: {
-        type: String,
-        required: true
+      type: String,
+      required: true
+    },
+    likesCounter: {
+      type: Number,
+      default: 0
     }
   },
   {
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,7 +5,19 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
-// // auth routes
-// router.use('/auth', require('./auth.routes'))
+// auth routes
+router.use('/auth', require('./auth.routes'))
+
+// movies routes
+router.use('/movies', require('./movies.routes'))
+
+// profile routes
+router.use('/profile', require('./profile.routes'))
+
+// shorts routes
+router.use('/shorts', require('./shorts.routes'))
+
+// valoration routes
+router.use('/valoration', require('./valoration.routes'))
 
 module.exports = router;
