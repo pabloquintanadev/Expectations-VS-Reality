b45ee769590adde615544bd0147b7cf94b7d17cc
mdelgado333 <101524023+mdelgado333@users.noreply.github.com>
2022-04-26 11:33:08 +0200

    mierda video

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -8,15 +8,7 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_SECRET
 });
 
-const storage = new CloudinaryStorage({
-  // cloudinary: cloudinary,
-  cloudinary,
-  params: {
-    allowed_formats: ['jpg', 'png'],
-    folder: 'movie-project' // The name of the folder in cloudinary
-    // resource_type: 'raw' => this is in case you want to upload other type of files, not just images
-  }
-});
+const storage = new CloudinaryStorage({cloudinary})
 
 //                     storage: storage
 module.exports = multer({ storage });
